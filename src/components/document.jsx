78de231b06973d580269541b1f9fd0f6830a9479
radhika9083 <ipisc0047@pandoconsultants.com>
2024-07-09 11:07:09 +0530

    update header section

--- src/components/document.jsx
+++ src/components/document.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import picture from '../assets/rafiki.png'
+import picture from '../../src/assets/rafiki.png'
 const DocumentSolution = () => {
   return (
-    <div className=" justify-center h-screen  max-w-[1240px] flex items-center">
-      <div className="flex flex-col md:flex-row bg-white rounded-lg p-8 space-x-4 ml">
-        <div className="flex items-center justify-center flex-1">
+    <div className="h-screen bg-white max-w-full flex justify-center sm:justify-center ">
+      <div className="flex lg:flex-row flex-col">
+        <div className="mx-10 my-20 sm:justify-center">
           <img src={picture} alt="Documents" className="w-full h-auto" />
         </div>
-        <div className="flex-1 text-center md:text-left">
-          <h2 className="text-2xl font-bold mt-10 lg:text-wrap md:text-balance ">Your one step Solution for all Documents Need 100% Free & Effortless</h2>
-          <button className="bg-[#44B7BC] text-white py-2 px-4 rounded-2xl hover:bg-[#44B7BC] my-11">Know More</button>
+        <div className=" flex flex-col justify-center items-center">
+          <div className="mt-5 lg:w-[479px] font-Poppins">
+          <h2 className="text-[36px] font-Poppins  text-wrap sm:text-wrap">Your one step Solution for all Documents Need 100% Free & Effortless</h2>
+          <button className="bg-[#44B7BC] font-Poppins  text-white py-2 px-4 rounded-2xl hover:bg-[#44B7BC] my-11">Know More</button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
- 
+
 export default DocumentSolution;
- 
