f723f692375320f15e2cd2ac0251a4c2eba0de42
komal345-ai <ipisc0045@pandoconsultants.com>
2024-07-22 18:51:23 +0530

    Added RemovePages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainLayout from './components/MainLayout';
 import SplitPage from './components/SplitPage';
 import MergePDF from './components/Mergepdf';
 import PageNotFound from './components/PageNotFound';
+import RemovePages from './components/RemovePages';
 
  function App() {
   return (
@@ -12,6 +13,7 @@ import PageNotFound from './components/PageNotFound';
       <Route path="/" element={<MainLayout />} />
       <Route path="/split" element={<SplitPage />} />
       <Route path="/merge" element={<MergePDF/>}/>
+      <Route path="/remove" element={<RemovePages/>}/>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
     </Router>
--- src/components/PlayStore.jsx
+++ src/components/PlayStore.jsx
@@ -3,18 +3,19 @@ import picture1 from '../assets/img_play_store.png'
 const PlayStore = () => {
   return (
 
-    <div className='flex justify-center flex-wrap'>
+    <div className='flex md:justify-center flex-wrap'>
       <div className="flex flex-col lg:flex-row items-center w-full justify-center  bg-white">
         <div className="mb-6 lg:mb-0 lg:mr-6 sm:w-1/2 flex lg:justify-center w-full ">
           <img src={picture1} alt="Google Play" className="w-[42rem] h-[17rem] lg:p-10 pt-10" />
         </div>
-        <div className="text-center lg:text-left text-xl w-1/2 my-[2rem] lg:pr-40 ">
+      <div className="text-center text-sm lg:text-left md:text-xl w-1/2 my-[2rem] lg:pr-40 ">
           <h2 className="text-3xl lg:text-3xl font-bold lg:mb-4 lg:mt-[3rem] leading-10 font-Poppins ">
-            You Can Download Our App <br/>From Play Store
+            You Can Download Our App From Play Store
           </h2>
           <div className=' lg:w-[30vw] '>
-          <p className="text-black text-2xl mt-[3rem] mb-[3rem] leading-9 font-Poppins text-wrap font-semibold ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <p className="text-black text-2xl mt-[3rem] mb-[3rem] leading-9 font-Poppins font-semibold text-balance sm:ml-5 md:ml-5 xl:ml-5 lg:line-clamp">
+          Download Easy Documents on the Play Store!
+          We're excited to launch Easy Documents, our new app for seamless document management. Get it now on the Play Store and stay organized effortlessly.
           </p>
           </div>
           <button className="bg-[#44B7BC] hover:bg-[#30aab1]  text-white font-semibold py-2 px-11 rounded-full ">
@@ -27,3 +28,4 @@ const PlayStore = () => {
 };
 
 export default PlayStore;
+
