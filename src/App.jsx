9f88b2b3b644f05dbcf7b0f5dbebf53fef50f943
komal345-ai <ipisc0045@pandoconsultants.com>
2024-07-30 10:25:46 +0530

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import MainLayout from './components/MainLayout';
-
 import MergePDF from './components/Mergepdf';
-
-
 import Navbar from './components/Navbar'
-
 import PageNotFound from './components/PageNotFound';
-
 import RemovePages from './components/RemovePages';
-import Footer from './components/Footer';
+
 import PdfToDocs from './components/PdfToDocs';
 import FileUpload from './components/FileUpload';
 
@@ -22,16 +17,13 @@ function App() {
   return (
     <Router>
        <Navbar/>
-
       <Routes>
       <Route path="/" element={<MainLayout />} />
-      <Route path="/split" element={<FileUpload />} />
+      <Route path="/split" element={<FileUpload name={splitprops}/>} />
       <Route path="/merge" element={<MergePDF/>}/>
-
       <Route path="/remove" element={<RemovePages/>}/>
       <Route path="/convert" element={<PdfToDocs />} />
-      <Route path="*" element={<PageNotFound />} />
-      
+      <Route path="*" element={<PageNotFound />} />   
     </Routes>
     <Footer />
     </Router>
