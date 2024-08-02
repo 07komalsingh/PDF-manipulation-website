da83ccfedf08edb0bdd7908a850007cad71f4d8a
radhika9083 <ipisc0047@pandoconsultants.com>
2024-08-02 18:08:54 +0530

    pdfAdding

--- src/components/Adding.jsx
+++ src/components/Adding.jsx
@@ -7,13 +7,13 @@ import group from "../assets/img_gup.png";
 import toastr from "toastr";
 import "toastr/build/toastr.min.css";
 import FileInput from './FileInput';
-import { RiDeleteBin5Fill } from "react-icons/ri";
- 
+import { RiDeleteBin5Fill, RiDragMove2Fill } from "react-icons/ri";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 // Set up pdfjs worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 toastr.options = {
-
   closeButton: true,
   progressBar: true,
   timeOut: "3000",
@@ -21,70 +21,84 @@ toastr.options = {
   preventDuplicates: true,
   newestOnTop: true,
 };
+
 const Adding = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [fileURLs, setFileURLs] = useState([]);
   const [mergedPDFUrl, setMergedPDFUrl] = useState(null);
- 
+
   const onFilesSelected = (files) => {
     setSelectedFiles((prevFiles) => [...prevFiles, ...files]);
     const newUrls = files.map((file) => URL.createObjectURL(file));
     setFileURLs((prevUrls) => [...prevUrls, ...newUrls]);
     setMergedPDFUrl(null); // Clear merged PDF URL when new files are selected
   };
- 
+
   const handleRemoveFile = (index) => {
     const newSelectedFiles = [...selectedFiles];
     const newFileURLs = [...fileURLs];
- 
+
     newSelectedFiles.splice(index, 1);
     newFileURLs.splice(index, 1);
- 
+
     setSelectedFiles(newSelectedFiles);
     setFileURLs(newFileURLs);
   };
- 
+
   const handleMerge = async () => {
     try {
       const mergedPdf = await PDFDocument.create();
- 
+
       for (const file of selectedFiles) {
         const arrayBuffer = await file.arrayBuffer();
         const pdf = await PDFDocument.load(arrayBuffer);
         const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
         copiedPages.forEach((page) => mergedPdf.addPage(page));
       }
- 
+
       const mergedPdfFile = await mergedPdf.save();
       const mergedPdfBlob = new Blob([mergedPdfFile], {
         type: "application/pdf",
       });
       const mergedPdfUrl = URL.createObjectURL(mergedPdfBlob);
       setMergedPDFUrl(mergedPdfUrl);
- 
+
       toastr.success("PDF merged successfully!", "Success");
     } catch (error) {
       toastr.error("Failed to merge PDFs.", "Error");
     }
   };
- 
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(fileURLs);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    const reorderedFiles = Array.from(selectedFiles);
+    const [reorderedFile] = reorderedFiles.splice(result.source.index, 1);
+    reorderedFiles.splice(result.destination.index, 0, reorderedFile);
+
+    setFileURLs(items);
+    setSelectedFiles(reorderedFiles);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center mt-5 pb-14">
+    <div className="flex flex-col items-center  mt-5 pb-14 bg-[#F5F5F5]">
       {selectedFiles.length < 1 ? (
         <div className="mb-4">
           <h2 className="text-4xl font-semibold mb-16 text-center">Merge PDF Files</h2>
           <h2 className="text-2xl font-semibold font-poppins mb-5 text-center">Upload Document</h2>
-          <div className="bg-[#E0F2F3B8] border-2 border-[#44B7BC] rounded-2xl xl:w-[70rem] lg:w-[50rem] px-3 md:w-[35rem] h-[23rem] flex justify-center items-center">
+          <div className="bg-gradient-to-r bg-[#E0F2F3B8] border-2 border-[#44B7BC] text-white rounded-2xl xl:w-[70rem] lg:w-[50rem] px-3 md:w-[35rem] h-[23rem] flex justify-center items-center shadow-lg">
             <div>
-              <h1 className="text-[#060808] font-poppins text-2xl font-normal text-center">
+              <h1 className="text-gray-800 font-poppins text-2xl font-normal text-center">
                 Upload PDF Attachments
               </h1>
               <div className="flex flex-col items-center">
-                <div>
-                  <img src={group} alt="PDF Icon" className="h-20 mt-4 w-full md:w-auto" />
-                </div>
-                <FileInput onFilesSelected={onFilesSelected} useIcon={false} className="rounded-full" />
-                <div className="flex flex-col text-gray-600 mt-[1rem] font-poppins">
+                <img src={group} alt="PDF Icon" className="h-20 mt-4 w-full md:w-auto" />
+                <FileInput onFilesSelected={onFilesSelected} useIcon={false} className="rounded-full mt-4" />
+                <div className="flex flex-col text-gray-600 mt-4 font-poppins">
                   <h1 className="text-2xl">Choose your PDF files here</h1>
                 </div>
               </div>
@@ -93,26 +107,50 @@ const Adding = () => {
         </div>
       ) : (
         <div className="flex flex-col items-center justify-center sm:mb-3">
-          <div className="flex flex-wrap justify-center">
-            {fileURLs.map((url, index) => (
-              <div key={index} className="relative mb-8 mx-2 bg-white shadow-md rounded-lg p-4">
-                <button
-                  onClick={() => handleRemoveFile(index)}
-                  className="absolute top-0 right-0  text-red-500 hover:text-red-700"
-                ><RiDeleteBin5Fill className="w-5 h-5"/>
-                </button>
-                <Document file={url}>
-                  <Page pageNumber={1} width={250} />
-                </Document>
-              </div>
-            ))}
-            <div className="mb-8 mx-2 bg-white shadow-md rounded-lg py-8 px-9 flex items-center justify-center sm:w-[250px] w-[280px]">
-              <FileInput onFilesSelected={onFilesSelected} useIcon={true} />
-            </div>
-          </div>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="droppable" direction="horizontal">
+              {(provided) => (
+                <div
+                  className="grid grid-cols-4 gap-4 p-4"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {fileURLs.map((url, index) => (
+                    <Draggable key={index} draggableId={index.toString()} index={index}>
+                      {(provided) => (
+                        <div
+                          className=" bg-white shadow-lg rounded-lg p-4 transition-transform duration-300 ease-in-out transform hover:scale-105 border border-gray-200"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <button
+                            onClick={() => handleRemoveFile(index)}
+                            className="absolute top-0 right-0 m-2 text-red-500 hover:text-red-700"
+                          >
+                            <RiDeleteBin5Fill className="w-5 h-5" />
+                          </button>
+                          <div className="flex items-center justify-between">
+                            <Document file={url}>
+                              <Page pageNumber={1} width={130} />
+                            </Document>
+                            <RiDragMove2Fill className="w-6 h-6 text-gray-500 cursor-grab" />
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  <div className=" mx-2 bg-white shadow-lg rounded-lg py-6 px-9 flex items-center justify-center">
+                    <FileInput onFilesSelected={onFilesSelected} useIcon={true} />
+                  </div>
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
           <div className="flex items-center sm:mt-4">
             <button
-              className={`bg-[#44B7BC] hover:bg-[#30aab1] text-white font-semibold py-2 px-24 rounded-full ${
+              className={`bg-gradient-to-r bg-[#44B7BC] hover:bg-[#30aab1]  text-white font-semibold py-2 px-24 rounded-full shadow-md transition duration-300 ${
                 selectedFiles.length < 2 ? "opacity-50 cursor-not-allowed" : ""
               }`}
               onClick={handleMerge}
@@ -124,7 +162,7 @@ const Adding = () => {
           {mergedPDFUrl && (
             <a
               href={mergedPDFUrl}
-              className="bg-[#44B7BC] hover:bg-[#30aab1] text-white font-semibold py-2 sm:px-[82px] px-20 rounded-full mt-4"
+              className="bg-gradient-to-r bg-[#44B7BC] hover:bg-[#30aab1] text-white font-semibold py-2 px-20 rounded-full mt-4 shadow-md transition duration-300"
               download="merged.pdf"
             >
               Download PDF
@@ -135,7 +173,8 @@ const Adding = () => {
     </div>
   );
 };
- 
+
 export default Adding;
+
  
  
